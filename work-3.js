4114c17c64afe1519f581d8e2c9e5443a11ab02c
moonhr <moonhr153@gmail.com>
2024-06-03 12:05:08 +0900

    modify f workThree 로직 변경

--- work-3.js
+++ work-3.js
@@ -1,8 +1,12 @@
 console.log('work-3');
 
-function workThree(a,b){
-  let local = {}
-  local[a] = a;
-  local[b] = b;
+function workThree(index, name) {
+  let local = {
+    number: index + 1,
+    name: name,
+  };
   return local;
 };
+
+let test = workThree(5, '문혜림');
+console.log(test)
